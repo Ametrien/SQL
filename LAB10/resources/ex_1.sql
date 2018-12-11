58a8176c7de73188051e18d9b02ebced24170274
AppleAirPods <43112986+AppleAirPods@users.noreply.github.com>
2018-12-11 14:50:25 +0200

    Add files via upload

---
+++ LAB10/resources/ex_1.sql
@@ -0,0 +1,15 @@
+CREATE OR ALTER TRIGGER inregistrarea_noua ON Orarul 
+AFTER  UPDATE
+AS
+BEGIN
+DECLARE @old INT;
+DECLARE @new INT;
+SELECT TOP 1 @old = Auditoriu FROM deleted
+SELECT TOP 1 @new = Auditoriu FROM inserted
+SELECT TOP 1 *, @old AS Old_Aud FROM orarul
+END
+GO
+
+UPDATE orarul 
+SET orarul.Auditoriu = 205
+WHERE Auditoriu = 501;
